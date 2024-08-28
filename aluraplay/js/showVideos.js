456d22cb6185d1d6295572282550a0d2f4ae3567
Thierry Chatauan <thycbs@gmail.com>
2024-08-28 20:48:51 +0100

    Creating js files

---
+++ aluraplay/js/showVideos.js
@@ -0,0 +1,33 @@
+import { connectionApi } from "./connectionApi.js";
+
+const list = document.querySelector("[data-list]");
+
+function createCard(title, description, url, image) {
+  const video = document.createElement("li");
+  video.className = "videos__item";
+  video.innerHTML = ` <iframe width="100%" height="72%" src="${url}"
+                title="${title}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="description-video">
+                <img src="${image}" alt="logo canal alura">
+                <h3>${title}</h3>
+                <p>${description}</p>
+            </div>`;
+
+  return video;
+}
+
+async function toListVideos() {
+  const listApi = await connectionApi.toListVideos();
+  listApi.forEach(element => {list.appendChild(
+    createCard(element.title, 
+               element.description, 
+               element.url, 
+               element.image
+            ))
+    
+  });
+}
+
+toListVideos()
