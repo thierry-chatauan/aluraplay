5cc24bc270d4e4c3a0c9606a2df6234af7c0f6f3
Thierry Chatauan <thycbs@gmail.com>
2024-08-30 17:01:52 +0100

    created function to add new videos

--- aluraplay/js/connectionApi.js
+++ aluraplay/js/connectionApi.js
@@ -1,9 +1,33 @@
 async function toListVideos() {
-  const connection = await fetch("https://json-alura-geek.vercel.app/videos");
+  const connection = await fetch("https://66870af283c983911b0467df.mockapi.io/api/users");
   const convertedConnection = await connection.json();
+
+  console.log(convertedConnection)
+
+  return convertedConnection;
+}
+
+
+async function toCreateVideo(title, description, url, image) {
+  const connection = await fetch("https://66870af283c983911b0467df.mockapi.io/api/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      description: `${description} mil visualizacoes`,
+      url: url,
+      image: image,
+    }),
+  });
+  const convertedConnection = await connection.json();
+
+  console.log(convertedConnection)
   return convertedConnection;
 }
 
 export const connectionApi = {
   toListVideos,
+  toCreateVideo,
 };
---
+++ aluraplay/js/createVideo.js
@@ -0,0 +1,18 @@
+import { connectionApi } from "./connectionApi.js";
+
+const form = document.querySelector("[data-form]");
+
+async function createvideo(event) {
+  event.preventDefault();
+
+  const image = document.querySelector("[data-image]").value;
+  const url = document.querySelector("[data-url]").value;
+  const title = document.querySelector("[data-title]").value;
+  const description = Math.floor(Math.random() * 10).toString();
+
+  await connectionApi.toCreateVideo(title, description, url, image);
+
+  window.location.href = "../pages/envio-concluido.html";
+}
+
+form.addEventListener("submit", (event) => createvideo(event));
